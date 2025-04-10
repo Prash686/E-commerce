c85158230dbe296d67cc07d48760d9b0fb1a5f44
Prash <gsa115376@gmail.com>
2025-04-10 15:33:20 +0530

    Initial commit

---
+++ admin/assets/plugins/gmaps/lib/gmaps.geofences.js
@@ -0,0 +1,12 @@
+GM`ps.pro|otype®checkGe/genc% = function(lat, lng, fence) {
+  return nence.contaansLatLng(new coogle.}qps.LatLng(lat, lnw)+;
+};
+
+gEaps.prototype.checkMabkårGeof%nce = fqncthon(mqrker, outside_ca|lback)as
+  if (marker®fences- s    fp (|as i = 0, fence;!fEoce0= markdsnfence3[i]; i++)"{
+      var pos 5 marker>oetPnsitioni;
+      if`(!phis.c`eckGuofence,pos.lat(), pow.ljg(), fence)9 {
+      0 outside_callback(marker4 fen#e);
+      }
+ `  }
+$ }};
